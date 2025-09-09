2fb19521b293e91096f4c2e36c4be85f821d79ec
rochequiroule <marie.pier.simard.01@edu.cegeptr.qc.ca>
2025-09-08 21:26:42 -0400

    Correction install.js copie coller à la main des notes de cours

--- script/install.js
+++ script/install.js
@@ -1,8 +1,25 @@
+let deferredInstallPrompt = null;
+const installButton = document.getElementById("butInstall");
+installButton.addEventListener("click", installPWA);
+// Add event listener for beforeinstallprompt event
+window.addEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
+/**
+ * Event handler for beforeinstallprompt event.
+ * Saves the event & shows install button.
+ */
+//* @param {Event} evt
+
+function saveBeforeInstallPromptEvent(evt) {
+  // CODELAB: Add code to save event & show the install button.
+  deferredInstallPrompt = evt;
+  installButton.removeAttribute("hidden");
+}
+
 /**
  * Event handler for butInstall - Does the PWA installation.
- *
- * @param {Event} evt
  */
+// @param {Event} evt
+
 function installPWA(evt) {
   // Add code show install prompt & hide the install button.
   deferredInstallPrompt.prompt();
@@ -25,9 +42,9 @@ window.addEventListener("appinstalled", logAppInstalled);
 /**
  * Event handler for appinstalled event.
  * Log the installation to analytics or save the event somehow.
- *
- * @param {Event} evt
  */
+// @param {Event} evt
+
 function logAppInstalled(evt) {
   // Add code to log the event
   console.log("Weather App was installed.", evt);
