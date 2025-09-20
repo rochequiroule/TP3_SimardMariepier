cf8fb5f5e609f03df3b00c7f37e086ae2df1f022
rochequiroule <marie.pier.simard.01@edu.cegeptr.qc.ca>
2025-09-20 17:03:09 -0400

    Ajustement au niveau de service-worker pour ajouter fichier en cache nécessaire

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,44 @@
 // Nom du cache — change ce nom si tu veux forcer une mise à jour
-const CACHE_NAME = "static-cache-v7";
+const CACHE_NAME = "static-cache-v8";
 
 // Fichiers à mettre en cache au moment de l’installation
-const FILES_TO_CACHE = ["/TP3_SimardMariepier/offline.html", "/TP3_SimardMariepier/index.html", "/TP3_SimardMariepier/style/css/style.css"];
+const FILES_TO_CACHE = [
+  "/TP3_SimardMariepier/offline.html",
+  "/TP3_SimardMariepier/index.html",
+  "/TP3_SimardMariepier/style/css/style.css",
+  "/TP3_SimardMariepier/contact.html",
+  "/TP3_SimardMariepier/confirmation-form.html",
+  "/TP3_SimardMariepier/confirmation-infolettre.html",
+  "/TP3_SimardMariepier/realisation.html",
+  "/TP3_SimardMariepier/script/animation.js",
+  "/TP3_SimardMariepier/script/script.js",
+  "/TP3_SimardMariepier/script/validationForm.js",
+  "/TP3_SimardMariepier/script/validationInfolettre.js",
+  "/TP3_SimardMariepier/image/1400x780-chaise-mur.jpg",
+  "/TP3_SimardMariepier/image/image-250x200-banc.jpg",
+  "/TP3_SimardMariepier/image/image-250x200-plancheadecouper.jpg",
+  "/TP3_SimardMariepier/image/image-500x200-bois.jpg",
+  "/TP3_SimardMariepier/image/realisation_01-c.jpg",
+  "/TP3_SimardMariepier/image/realisation_01-v.jpg",
+  "/TP3_SimardMariepier/image/realisation_01.jpg",
+  "/TP3_SimardMariepier/image/realisation_02-c.jpg",
+  "/TP3_SimardMariepier/image/realisation_02-v.jpg",
+  "/TP3_SimardMariepier/image/realisation_02.jpg",
+  "/TP3_SimardMariepier/image/realisation_03-c.jpg",
+  "/TP3_SimardMariepier/image/realisation_03-v.jpg",
+  "/TP3_SimardMariepier/image/realisation_03.jpg",
+  "/TP3_SimardMariepier/image/realisation_04-c.jpg",
+  "/TP3_SimardMariepier/image/realisation_04-v.jpg",
+  "/TP3_SimardMariepier/image/realisation_04.jpg",
+  "/TP3_SimardMariepier/image/realisation_05-c.jpg",
+  "/TP3_SimardMariepier/image/realisation_05-v.jpg",
+  "/TP3_SimardMariepier/image/realisation_05.jpg",
+  "/TP3_SimardMariepier/image/realisation_06-v.jpg",
+  "/TP3_SimardMariepier/image/realisation_06-c.jpg",
+  "/TP3_SimardMariepier/image/realisation_06.jpg",
+  "/TP3_SimardMariepier/image/ReBois_logo.svg",
+  "/TP3_SimardMariepier/video/4480575-hd_1920_1080_30fps.mp4",
+];
 
 // INSTALL
 self.addEventListener("install", (evt) => {
