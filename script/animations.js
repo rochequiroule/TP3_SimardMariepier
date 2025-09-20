54dfa34c7be89f95af74238ef8a0f48cb35cdf3c
rochequiroule <marie.pier.simard.01@edu.cegeptr.qc.ca>
2025-09-20 17:54:35 -0400

    Ajout de titre animé et d'animation on scroll dans l'ensemble de site

---
+++ script/animations.js
@@ -0,0 +1,22 @@
+var textWrapper = document.querySelector(".titleAnimate");
+
+// Remplacer chaque lettre par un <span>
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+// Lancer l’animation
+anime
+  .timeline({ loop: true })
+  .add({
+    targets: ".titleAnimate .letter",
+    opacity: [0, 1],
+    easing: "easeInOutQuad",
+    duration: 150,
+    delay: (el, i) => 50 * (i + 1),
+  })
+  .add({
+    targets: ".titleAnimate",
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 2200,
+  });
